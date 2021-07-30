8ef9ea069d9a3db3b3c934a49469e600432ff765
Akarsh2912 <72065369+Akarsh2912@users.noreply.github.com>
2021-07-30 09:49:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe" # Add Tesseract to Path
+pytesseract.pytesseract.tesseract_cmd = r"path" # Add Tesseract to Path
 
 img = Image.open('2.png')
 text = pytesseract.image_to_string(img)
